f87b2df3ed4bd2caac839b964ce63e01adea6dec
CastroOlympias <blackcarrera@msn.com>
2021-05-07 17:26:04 -0600

    Add ablity to invoke inquirer to log first question's answer into bash

--- app.js
+++ app.js
@@ -12,8 +12,15 @@ const inquirer = require('inquirer');
 
 
 
-
-console.log(inquirer)
+inquirer
+    .prompt([
+        {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
 
 
